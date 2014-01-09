196722ce08f3eb19e6042c2e725345f3bb9c096e
Petr Polezhaev <p_polezhaev@wargaming.net>
2014-01-09 17:45:02 +0400

    eliminator: Fix table constructors being broken into pieces

--- ljd/ast/eliminator.py
+++ ljd/ast/eliminator.py
@@ -212,23 +212,6 @@ class EliminatorVisitor(traverse.Visitor):
 		tablevar = node.destinations.contents[0]
 		slot = node.expressions.contents[0]
 
-		if not isinstance(slot, nodes.Identifier):
-			return
-
-		# Skip local variables
-		if slot.type != nodes.Identifier.T_SLOT:
-			return
-
-		description = self._get_slot_description(slot.slot)
-
-		# That's possible in a case of a logical expression -
-		# A global variable is assigned in the first instructin o
-		# the next block after the expression
-		if description is None:
-			return
-
-		value = description.assignment.expressions.contents[0]
-
 		if isinstance(tablevar.table, nodes.Identifier) and	\
 				tablevar.table.type == nodes.Identifier.T_SLOT:
 			tabledesc = self._get_slot_description(tablevar.table.slot)
@@ -241,6 +224,15 @@ class EliminatorVisitor(traverse.Visitor):
 			if not isinstance(constructor, nodes.TableConstructor):
 				return
 
+			value = slot
+
+			if isinstance(slot, nodes.Identifier):
+				info = self._get_slot_description(slot.slot)
+				
+				if info is not None:
+					srcs = info.assignment.expressions
+					value = srcs.contents[0]
+
 			record = nodes.TableRecord()
 			record.key = tablevar.key
 			record.value = value
@@ -248,8 +240,23 @@ class EliminatorVisitor(traverse.Visitor):
 			constructor.records.contents.append(record)
 			self._invalidate_node(node)
 		else:
+			if not isinstance(slot, nodes.Identifier):
+				return
+
+			# Skip local variables
+			if slot.type != nodes.Identifier.T_SLOT:
+				return
+
+			description = self._get_slot_description(slot.slot)
+
 			# Nothing to worry about
 			if len(description.slots) == 1:
+				return
+
+			# That's possible in a case of a logical expression -
+			# A global variable is assigned in the first instruction
+			# of the next block after the expression
+			if description is None:
 				return
 
 			dst = description.node
