6cab50b2da02f64b3c9e6abf22f41232267a5db3
Andrian Nord <NightNord@gmail.com>
2014-01-24 22:21:25 +0400

    builder: a bit of refactoring for a warps build process
    Now warps are built in their own fancy loop, before any other instruction
    is processed. This commit was inteded to fix some bugs with
    if bla then bla else end
    
    type statements (with an empty but preset else clause), but it turned out
    that it won't help, so this change isn't fixing anything. But it's a bit
    more "correct", so I'll commit it anyway

--- ljd/ast/builder.py
+++ ljd/ast/builder.py
@@ -68,6 +68,7 @@ def _build_function_arguments(state, prototype):
 
 def _build_function_blocks(state, instructions):
 	_blockenize(state, instructions)
+	_establish_warps(state, instructions)
 
 	state.blocks[0].warpins_count = 1
 
@@ -75,7 +76,7 @@ def _build_function_blocks(state, instructions):
 		addr = block.first_address
 		state.block = block
 
-		while addr <= block.last_address:
+		while addr <= block._last_body_addr:
 			instruction = instructions[addr]
 
 			statement = _build_statement(state, addr, instruction)
@@ -90,23 +91,12 @@ def _build_function_blocks(state, instructions):
 
 			addr += 1
 
-		next_address = block.last_address + 1
+		if len(block.contents) == 0:
+			continue
 
-		if block.warp is None:
-			if block != state.blocks[-1]:
-				block.warp = nodes.UnconditionalWarp()
-				block.warp.type = nodes.UnconditionalWarp.T_FLOW
+		if isinstance(block.contents[0], nodes.BlackHole):
+			block.contents.append(block.contents.pop(0))
 
-				next_block = state._warp_in_block(next_address)
-				block.warp.target = next_block
-			else:
-				block.warp = nodes.EndWarp()
-
-		setattr(block.warp, "_addr", block.last_address)
-
-	state.blocks[-1].warp = nodes.EndWarp()
-
-	# Blocks are linked with warps
 	return state.blocks
 
 
@@ -139,10 +129,6 @@ def _blockenize(state, instructions):
 		if opcode not in _WARP_INSTRUCTIONS:
 			addr += 1
 			continue
-
-		# We can't process the warp instructions here because of the
-		# copy-ifs - they need to emit a statement into the block
-		# before creating a warp into another
 
 		if opcode in _JUMP_WARP_INSTRUCTIONS:
 			destination = get_jump_destination(addr, instruction)
@@ -181,19 +167,196 @@ def _blockenize(state, instructions):
 		index += 1
 
 
+def _establish_warps(state, instructions):
+	state.blocks[0].warpins_count = 1
+
+	for block in state.blocks[:-1]:
+		state.block = block
+
+		end_addr = block.last_address + 1
+		start_addr = max(block.last_address - 1, block.first_address)
+
+		warp = instructions[start_addr:end_addr]
+
+		block.warp, shift = _build_warp(state, block.last_address, warp)
+
+		setattr(block, "_last_body_addr", block.last_address - shift)
+		setattr(block.warp, "_addr", block.last_address - shift + 1)
+
+	last_block = state.blocks[-1]
+	last_block.warp = nodes.EndWarp()
+
+	setattr(last_block, "_last_body_addr", last_block.last_address)
+	setattr(last_block.warp, "_addr", last_block.last_address)
+
+
+def _build_warp(state, last_addr, instructions):
+	last = instructions[-1]
+
+	if last.opcode in (ins.JMP.opcode, ins.UCLO.opcode, ins.ISNEXT.opcode):
+		return _build_jump_warp(state, last_addr, instructions)
+
+	elif last.opcode >= ins.ITERL.opcode and last.opcode <= ins.JITERL.opcode:
+		assert len(instructions) == 2
+		return _build_iterator_warp(state, last_addr, instructions)
+
+	elif last.opcode >= ins.FORL.opcode and last.opcode <= ins.JFORL.opcode:
+		return _build_numeric_loop_warp(state, last_addr, last)
+
+	else:
+		return _build_flow_warp(state, last_addr, last)
+
+
+def _build_jump_warp(state, last_addr, instructions):
+	last = instructions[-1]
+	opcode = 256 if len(instructions) == 1 else instructions[-2].opcode
+
+	if opcode <= ins.ISF.opcode:
+		assert last.opcode != ins.ISNEXT.opcode
+		return _build_conditional_warp(state, last_addr, instructions)
+	else:
+		return _build_unconditional_warp(state, last_addr, last)
+
+
+def _build_conditional_warp(state, last_addr, instructions):
+	condition = instructions[-2]
+	condition_addr = last_addr - 1
+
+	warp = nodes.ConditionalWarp()
+
+	if condition.opcode in (ins.ISTC.opcode, ins.ISFC.opcode):
+		expression = _build_unary_expression(state,
+							condition_addr,
+							condition)
+
+		setattr(warp, "_slot", condition.A)
+	elif condition.opcode >= ins.IST.opcode:
+		expression = _build_unary_expression(state,
+							condition_addr,
+							condition)
+
+		setattr(warp, "_slot", condition.CD)
+	else:
+		expression = _build_comparison_expression(state,
+							condition_addr,
+							condition)
+
+	warp.condition = expression
+
+	jump = instructions[-1]
+	jump_addr = last_addr
+
+	destination = get_jump_destination(jump_addr, jump)
+
+	# A condition is inverted during the preparation phase above
+	warp.false_target = state._warp_in_block(destination)
+	warp.true_target = state._warp_in_block(jump_addr + 1)
+
+	# something or true or simply true
+	if warp.false_target == warp.true_target:
+		target = warp.false_target
+
+		black_hole = nodes.BlackHole()
+		black_hole.contents.append(warp.condition)
+
+		state.block.contents.append(black_hole)
+
+		warp = nodes.UnconditionalWarp()
+		warp.type = nodes.UnconditionalWarp.T_FLOW
+		warp.target = target
+
+	return warp, 2
+
+
+def _build_unconditional_warp(state, addr, instruction):
+	warp = nodes.UnconditionalWarp()
+	warp.type = nodes.UnconditionalWarp.T_JUMP
+
+	opcode = instruction.opcode
+
+	warp.is_uclo = opcode == ins.UCLO.opcode
+
+	if warp.is_uclo and instruction.CD == 0:
+		# Not a jump
+		return _build_flow_warp(state, addr, instruction)
+	else:
+		destination = get_jump_destination(addr, instruction)
+		warp.target = state._warp_in_block(destination)
+
+	return warp, 1
+
+
+def _build_iterator_warp(state, last_addr, instructions):
+	iterator = instructions[-2]
+	iterator_addr = last_addr - 1
+
+	assert iterator.opcode in (ins.ITERC.opcode, ins.ITERN.opcode)
+
+	warp = nodes.IteratorWarp()
+
+	base = iterator.A
+
+	warp.controls.contents = [
+		_build_slot(state, iterator_addr, base - 3),  # generator
+		_build_slot(state, iterator_addr, base - 2),  # state
+		_build_slot(state, iterator_addr, base - 1)  # control
+	]
+
+	last_slot = base + iterator.B - 2
+
+	slot = base
+
+	while slot <= last_slot:
+		variable = _build_slot(state, iterator_addr - 1, slot)
+		warp.variables.contents.append(variable)
+		slot += 1
+
+	jump = instructions[-1]
+	jump_addr = last_addr
+
+	destination = get_jump_destination(jump_addr, jump)
+	warp.way_out = state._warp_in_block(jump_addr + 1)
+	warp.body = state._warp_in_block(destination)
+
+	return warp, 2
+
+
+def _build_numeric_loop_warp(state, addr, instruction):
+	warp = nodes.NumericLoopWarp()
+
+	base = instruction.A
+
+	warp.index = _build_slot(state, addr, base + 3)
+	warp.controls.contents = [
+		_build_slot(state, addr, base + 0),  # start
+		_build_slot(state, addr, base + 1),  # limit
+		_build_slot(state, addr, base + 2)  # step
+	]
+
+	destination = get_jump_destination(addr, instruction)
+	warp.body = state._warp_in_block(destination)
+	warp.way_out = state._warp_in_block(addr + 1)
+
+	return warp, 1
+
+
+def _build_flow_warp(state, addr, instruction):
+	warp = nodes.UnconditionalWarp()
+	warp.type = nodes.UnconditionalWarp.T_FLOW
+	warp.target = state._warp_in_block(addr + 1)
+
+	opcode = instruction.opcode
+	shift = 1 if opcode in (ins.FORI.opcode, ins.UCLO.opcode) else 0
+
+	return warp, shift
+
+
 def _build_statement(state, addr, instruction):
 	opcode = instruction.opcode
 	A_type = instruction.A_type
 
-	# Internal opcodes are stable, so we may freely assume their order
-	if opcode == ins.ISTC.opcode or opcode == ins.ISFC.opcode:
-		return _build_copy_if_statement(state, addr, instruction)
-
-	elif opcode <= ins.ISF.opcode:
-		return _prepare_conditional_warp(state, addr, instruction)
-
 	# Generic assignments - handle the ASSIGNMENT stuff below
-	elif A_type == ins.T_DST or A_type == ins.T_UV:
+	if A_type == ins.T_DST or A_type == ins.T_UV:
 		return _build_var_assignment(state, addr, instruction)
 
 	# ASSIGNMENT starting from MOV and ending at KPRI
@@ -221,91 +384,18 @@ def _build_statement(state, addr, instruction):
 	elif opcode >= ins.CALLM.opcode and opcode <= ins.CALLT.opcode:
 		return _build_call(state, addr, instruction)
 
-	elif opcode == ins.ITERC.opcode or opcode == ins.ITERN.opcode:
-		return _prepare_iterator_warp(state, addr, instruction)
-
 	elif opcode == ins.VARG.opcode:
 		return _build_vararg(state, addr, instruction)
-
-	elif opcode == ins.ISNEXT.opcode:
-		return _build_unconditional_warp(state, addr, instruction)
 
 	elif opcode >= ins.RETM.opcode and opcode <= ins.RET1.opcode:
 		return _build_return(state, addr, instruction)
 
-	elif opcode == ins.FORI.opcode:
-		return _build_numeric_loop_warp_stub(state, addr, instruction)
-
-	elif opcode >= ins.FORL.opcode and opcode <= ins.JFORL.opcode:
-		return _build_numeric_loop_warp(state, addr, instruction)
-
-	elif opcode >= ins.ITERL.opcode and opcode <= ins.JITERL.opcode:
-		return _finalize_iterator_warp(state, addr, instruction)
-
-	elif opcode >= ins.LOOP.opcode and opcode <= ins.JLOOP.opcode:
+	else:
+		assert opcode == ins.UCLO.opcode or (
+				opcode >= ins.LOOP.opcode
+						and opcode <= ins.JLOOP.opcode)
 		# Noop
 		return None
-
-	else:
-		assert opcode == ins.UCLO.opcode or opcode == ins.JMP.opcode
-		return _build_jump_warp(state, addr, instruction)
-
-
-def _prepare_conditional_warp(state, addr, instruction):
-	warp = nodes.ConditionalWarp()
-
-	if instruction.opcode >= ins.IST.opcode:
-		expression = _build_unary_expression(state, addr, instruction)
-		setattr(warp, "_slot", instruction.CD)
-	else:
-		expression = _build_comparison_expression(state, addr, instruction)
-
-	warp.condition = expression
-
-	assert state.block.warp is None
-	state.block.warp = warp
-
-
-def _finalize_conditional_warp(state, addr, instruction):
-	warp = state.block.warp
-	assert isinstance(warp, nodes.ConditionalWarp)
-
-	destination = get_jump_destination(addr, instruction)
-
-	# A condition is inverted during the preparation phase above
-	warp.false_target = state._warp_in_block(destination)
-	warp.true_target = state._warp_in_block(addr + 1)
-
-	# something or true or simply true
-	if warp.false_target == warp.true_target:
-		target = warp.false_target
-
-		black_hole = nodes.BlackHole()
-		black_hole.contents.append(warp.condition)
-
-		state.block.contents.append(black_hole)
-
-		warp = nodes.UnconditionalWarp()
-		warp.type = nodes.UnconditionalWarp.T_FLOW
-		warp.target = target
-
-		state.block.warp = warp
-
-
-def _build_copy_if_statement(state, addr, instruction):
-	# It's being used only for expressions, and we don't need
-	# useless assignments there. Slot eliminator will take care of the
-	# conditions, but we will keep the information about copy to distinguish
-	# expressions from ifs later
-
-	warp = nodes.ConditionalWarp()
-	warp.condition = _build_unary_expression(state, addr, instruction)
-	setattr(warp, "_slot", instruction.A)
-
-	assert state.block.warp is None
-	state.block.warp = warp
-
-	# return assignment
 
 
 def _build_var_assignment(state, addr, instruction):
@@ -418,39 +508,6 @@ def _build_table_mass_assignment(state, addr, instruction):
 	return assignment
 
 
-def _prepare_iterator_warp(state, addr, instruction):
-	warp = nodes.IteratorWarp()
-
-	base = instruction.A
-
-	warp.controls.contents = [
-		_build_slot(state, addr, base - 3),  # generator
-		_build_slot(state, addr, base - 2),  # state
-		_build_slot(state, addr, base - 1)  # control
-	]
-
-	last_slot = base + instruction.B - 2
-
-	slot = base
-
-	while slot <= last_slot:
-		variable = _build_slot(state, addr - 1, slot)
-		warp.variables.contents.append(variable)
-		slot += 1
-
-	assert state.block.warp is None
-	state.block.warp = warp
-
-
-def _finalize_iterator_warp(state, addr, instruction):
-	warp = state.block.warp
-	assert isinstance(warp, nodes.IteratorWarp)
-
-	destination = get_jump_destination(addr, instruction)
-	warp.way_out = state._warp_in_block(addr + 1)
-	warp.body = state._warp_in_block(destination)
-
-
 def _build_call(state, addr, instruction):
 	call = nodes.FunctionCall()
 	call.function = _build_slot(state, addr, instruction.A)
@@ -513,61 +570,6 @@ def _build_return(state, addr, instruction):
 		node.returns.contents.append(nodes.MULTRES())
 
 	return node
-
-
-def _build_numeric_loop_warp_stub(state, addr, instruction):
-	warp = nodes.UnconditionalWarp()
-	warp.type = nodes.UnconditionalWarp.T_FLOW
-	warp.target = state._warp_in_block(addr + 1)
-
-	assert state.block.warp is None
-	state.block.warp = warp
-
-
-def _build_numeric_loop_warp(state, addr, instruction):
-	warp = nodes.NumericLoopWarp()
-
-	base = instruction.A
-
-	warp.index = _build_slot(state, addr, base + 3)
-	warp.controls.contents = [
-		_build_slot(state, addr, base + 0),  # start
-		_build_slot(state, addr, base + 1),  # limit
-		_build_slot(state, addr, base + 2)  # step
-	]
-
-	destination = get_jump_destination(addr, instruction)
-	warp.body = state._warp_in_block(destination)
-	warp.way_out = state._warp_in_block(addr + 1)
-
-	assert state.block.warp is None
-	state.block.warp = warp
-
-
-def _build_jump_warp(state, addr, instruction):
-	if state.block.warp is not None:
-		return _finalize_conditional_warp(state, addr, instruction)
-	else:
-		return _build_unconditional_warp(state, addr, instruction)
-
-
-def _build_unconditional_warp(state, addr, instruction):
-	warp = nodes.UnconditionalWarp()
-
-	opcode = instruction.opcode
-
-	warp.is_uclo = opcode == ins.UCLO.opcode
-
-	if warp.is_uclo and instruction.CD == 0:
-		# Not a jump
-		return
-	else:
-		warp.type = nodes.UnconditionalWarp.T_JUMP
-		destination = get_jump_destination(addr, instruction)
-		warp.target = state._warp_in_block(destination)
-
-	assert state.block.warp is None
-	state.block.warp = warp
 
 
 def _build_call_arguments(state, addr, instruction):
