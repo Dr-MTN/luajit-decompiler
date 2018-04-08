5edb3b22c1092d94d1bff71882c36f131c70effc
Alexandr Mitkalev <iroota@gmail.com>
2018-04-08 18:06:13 +0300

    Added an option to assign the output file

--- main.py
+++ main.py
@@ -25,6 +25,7 @@
 
 import sys
 import os
+import logging
 from optparse import OptionParser
 
 def dump(name, obj, level=0):
@@ -61,93 +62,154 @@ def dump(name, obj, level=0):
 			val = getattr(obj, key)
 			dump(key, val, level + 1)
 
+class MakeFileHandler(logging.FileHandler):
+    def __init__(self, filename, *args, **kwargs):
+        os.makedirs(os.path.dirname(filename), exist_ok=True)
+        logging.FileHandler.__init__(self, filename, *args, **kwargs)
 
-def main():
-	#parser arguments
-	parser = OptionParser()
+from datetime import datetime
 
-	parser.add_option("-f", "--file", \
-		type="string", dest="file_name", default="", \
-	        help="decompile file name", metavar="FILE")
-	parser.add_option("-o", "--output", \
-		type="string", dest="output_file", default="", \
+logger = logging.getLogger('LJD')
+logger.setLevel(logging.INFO)
+
+fh = MakeFileHandler(f'logs/{datetime.now().strftime("%Y_%m_%d_%H_%M_%S")}.log')
+fh.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+logger.addHandler(fh)
+
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
+console.setFormatter(formatter)
+logger.addHandler(console)
+
+
+class Main:
+	def main(self):
+		#parser arguments
+		parser = OptionParser()
+
+		parser.add_option("-f", "--file", \
+			type="string", dest="file_name", default="", \
+			help="decompile file name", metavar="FILE")
+		parser.add_option("-o", "--output", \
+			type="string", dest="output_file", default="", \
 			help="output file for writing", metavar="FILE")
-	parser.add_option("-j", "--jitverion", \
-		type="string", dest="luajit_version", default="2.1", \
-		help="luajit version, default 2.1, now support 2.0, 2.1")
+		parser.add_option("-j", "--jitverion", \
+			type="string", dest="luajit_version", default="2.1", \
+			help="luajit version, default 2.1, now support 2.0, 2.1")
+		parser.add_option("-r", "--recursive", \
+			type="string", dest="folder_name", default="", \
+			help="recursive decompile lua files", metavar="FOLDER")
+		parser.add_option("-d", "--dir_out", \
+			type="string", dest="folder_output", default="", \
+			help="directory to output decompiled lua scripts", metavar="FOLDER")
 
-	(options, args) = parser.parse_args()
-	if options.file_name == "":
-		print(options)
-		parser.error("options -f is required")
-	basepath=os.path.dirname(sys.argv[0])
-	if basepath == "":
-		basepath=".";
-	sys.path.append(basepath + "/ljd/rawdump/luajit/" + options.luajit_version + "/")
 
-	#because luajit version is known after argument parsed, so delay import modules
-	import ljd.rawdump.parser
-	import ljd.pseudoasm.writer
-	import ljd.ast.builder
-	import ljd.ast.validator
-	import ljd.ast.locals
-	import ljd.ast.slotworks
-	import ljd.ast.unwarper
-	import ljd.ast.mutator
-	import ljd.lua.writer
+		(self.options, args) = parser.parse_args()
+		basepath=os.path.dirname(sys.argv[0])
+		if basepath == "":
+			basepath=".";
+		sys.path.append(basepath + "/ljd/rawdump/luajit/" + self.options.luajit_version + "/")
 
-	file_in = options.file_name
+		#because luajit version is known after argument parsed, so delay import modules
+		import ljd.rawdump.parser
+		import ljd.pseudoasm.writer
+		import ljd.ast.builder
+		import ljd.ast.validator
+		import ljd.ast.locals
+		import ljd.ast.slotworks
+		import ljd.ast.unwarper
+		import ljd.ast.mutator
+		import ljd.lua.writer
 
-	header, prototype = ljd.rawdump.parser.parse(file_in)
+		self.ljd = ljd
 
-	if not prototype:
-		return 1
+		if self.options.folder_name:
+			for path, _, filenames in os.walk(self.options.folder_name):
+				for file in filenames:
+					if file.endswith('.lua'):
+						full_path = os.path.join(path, file)
 
-	# ljd.pseudoasm.writer.write(sys.stdout, header, prototype)
+						logger.info(full_path)
+						try:
+							self.decompile(full_path)
+							new_path = os.path.join(self.options.folder_output, os.path.relpath(full_path, self.options.folder_name))
+							os.makedirs(os.path.dirname(new_path), exist_ok=True)
+							self.write_file(new_path)
+							logger.info("Success")
+						except KeyboardInterrupt:
+							logger.info("Exit")
+							return 0
+						except:
+							logger.info("Exception")
+							logger.debug('', exc_info=True)
 
-	ast = ljd.ast.builder.build(prototype)
+			return 0
 
-	assert ast is not None
+		if self.options.file_name == "":
+			print(self.options)
+			parser.error("options -f is required")
 
-	ljd.ast.validator.validate(ast, warped=True)
+		self.decompile(self.options.file_name)
 
-	ljd.ast.mutator.pre_pass(ast)
+		if self.options.output_file:
+			self.write_file(self.options.output_file)
+		else:
+			self.ljd.lua.writer.write(sys.stdout, self.ast)
 
-	# ljd.ast.validator.validate(ast, warped=True)
+		return 0
 
-	ljd.ast.locals.mark_locals(ast)
+	def write_file(self, file_name):
+		with open(file_name, "w", encoding="utf8") as out_file:
+			self.ljd.lua.writer.write(out_file, self.ast)
 
-	# ljd.ast.validator.validate(ast, warped=True)
 
-	ljd.ast.slotworks.eliminate_temporary(ast)
+	def decompile(self, file_in):
+		header, prototype = self.ljd.rawdump.parser.parse(file_in)
 
-	# ljd.ast.validator.validate(ast, warped=True)
+		if not prototype:
+			return 1
 
-	if True:
-		ljd.ast.unwarper.unwarp(ast)
+		# self.ljd.pseudoasm.writer.write(sys.stdout, header, prototype)
 
-		# ljd.ast.validator.validate(ast, warped=False)
+		self.ast = self.ljd.ast.builder.build(prototype)
+
+		assert self.ast is not None
+
+		self.ljd.ast.validator.validate(self.ast, warped=True)
+
+		self.ljd.ast.mutator.pre_pass(self.ast)
+
+		# self.ljd.ast.validator.validate(self.ast, warped=True)
+
+		self.ljd.ast.locals.mark_locals(self.ast)
+
+		# ljd.ast.validator.validate(ast, warped=True)
+
+		self.ljd.ast.slotworks.eliminate_temporary(self.ast)
+
+		# self.ljd.ast.validator.validate(ast, warped=True)
 
 		if True:
-			ljd.ast.locals.mark_local_definitions(ast)
+			self.ljd.ast.unwarper.unwarp(self.ast)
 
-			# ljd.ast.validator.validate(ast, warped=False)
+			# self.ljd.ast.validator.validate(ast, warped=False)
 
-			ljd.ast.mutator.primary_pass(ast)
+			if True:
+				self.ljd.ast.locals.mark_local_definitions(self.ast)
 
-			ljd.ast.validator.validate(ast, warped=False)
+				# self.ljd.ast.validator.validate(self.ast, warped=False)
 
-	if options.output_file:
-		with open(options.output_file, "w", encoding="utf8") as out_file:
-			ljd.lua.writer.write(out_file, ast)
-	else:
-		ljd.lua.writer.write(sys.stdout, ast)
+				self.ljd.ast.mutator.primary_pass(self.ast)
 
-	return 0
+				self.ljd.ast.validator.validate(self.ast, warped=False)
 
 
 if __name__ == "__main__":
-	retval = main()
+	main_obj = Main()
+	retval = main_obj.main()
 	sys.exit(retval)
 
 # vim: ts=8 noexpandtab nosmarttab softtabstop=8 shiftwidth=8
