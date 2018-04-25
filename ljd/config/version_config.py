fb81340e3d10694b160b26a6c222de8e35d01d3c
Aussiemon <https://github.com/Aussiemon>
2018-04-25 10:28:31 -0600

    Added unversioned version_config

---
+++ ljd/config/version_config.py
@@ -0,0 +1,7 @@
+## Will automatically be changed if file is listed below
+use_version = 2.1
+
+## Add fully-qualified names and versions of lua files below
+_LUA_FILE_VERSIONS = {
+    ## './test/action_utils.lua': 2.1
+}
