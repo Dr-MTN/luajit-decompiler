Dr-MTN/luajit-decompiler

commit 7eae505bc0bb097c5ef3caca7b472ce84e73fed6

Change method of finding the difference between an if block and an expression, now using the slot types

ljd/ast/unwarper.py
@@ -521,8 +521,11 @@ def _find_expressions(start, body, end):
             # really a subexpression-as-operand
             end_warp = endest_end.warp
 
-            if not isinstance(end_warp, nodes.ConditionalWarp):
-                return expressions
+            # If any of the subexpressions are put into local variables, then this
+            #  must be an if block, rather than an expression.
+            for _, _, sub_slot, sub_slot_type in subs:
+                if sub_slot_type == nodes.Identifier.T_LOCAL:
+                    return expressions
 
             expressions = subs + expressions
             i = new_i

commit ad1954734f46472230cf72142ef83cb4d5e7d8b3

Fix some if statements being treated as if they were expressions - caused by 7eae505

ljd/ast/unwarper.py
@@ -558,9 +558,34 @@ def _find_expressions(start, body, end):
 
             # If any of the subexpressions are put into local variables, then this
             #  must be an if block, rather than an expression.
-            for _, _, sub_slot, sub_slot_type in subs:
+            for sub_start, sub_end, sub_slot, sub_slot_type in subs:
                 if sub_slot_type == nodes.Identifier.T_LOCAL:
                     return expressions
+
+                # Search the body of the subexpression - if it contains anything other
+                # than assignments, then it must be an if..end rather than an expression
+                #
+                # Here's an example of something that fails to decompile without this:
+                #
+                # local myvar = nil
+                # if test_var then
+                #         unrelated_func()
+                #         myvar = (myvar or f()) + var
+                # else
+                #         unrelated_func()
+                #         myvar = (myvar or f()) + var
+                # end
+
+                # Fetch the subexpression body
+                sub_start_i = extbody.index(sub_start)
+                sub_end_i = extbody.index(sub_end)
+                sub_body = extbody[sub_start_i:sub_end_i]
+
+                # And check it
+                for block in sub_body:
+                    for item in block.contents:
+                        if not isinstance(item, nodes.Assignment):
+                            return expressions
 
             expressions = subs + expressions
             i = new_i