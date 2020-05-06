f14a841b779db5fb9973fcc52174424a87d7a647
Campbell Suter <znix@znix.xyz>
2020-05-06 12:31:50 +1200

    Make the massive_std test pass massive refs into functions, as well as global variables

--- test/tests/massive_std.lua
+++ test/tests/massive_std.lua
@@ -1,9 +1,13 @@
 -- Test for the 'standard' massive refs in slotworks - varargs and functions.
 -- Nils are special, and handled in massive_nils.lua
 
+-- Put print in a local to avoid cluttering the bytecode up with GGETs
+local print = nil
+
 local a, b = f()
-gbl1 = a
-gbl2 = a
+print(a)
+print(a)
+print(b)
 
 local c, d = ...
 gbl3 = c
