ed566dd8ac4a99dabc2f5391218fb2a86adcd299
Campbell Suter <znix@znix.xyz>
2020-05-09 17:22:29 +1200

    Add helpers for getting ANSI colour control codes as strings to the test util Colour enum

--- test/utils.py
+++ test/utils.py
@@ -2,6 +2,10 @@ import sys
 from enum import Enum
 
 
+def sgr(code: str) -> str:
+    return "\x1b[%sm" % code
+
+
 class Colour(Enum):
     BLACK = 0
     RED = 1
@@ -11,11 +15,17 @@ class Colour(Enum):
     BLUE = 4
     WHITE = 7
 
+    def fg(self):
+        return "38;5;%d" % self.value
+
+    def bg(self):
+        return "48;5;%d" % self.value
+
     def set_fg(self):
-        Colour._sgr("38;5;%d" % self.value)
+        Colour._sgr(self.fg())
 
     def set_bg(self):
-        Colour._sgr("48;5;%d" % self.value)
+        Colour._sgr(self.bg())
 
     def print(self, text):
         self.write(text + "\n")
@@ -31,4 +41,4 @@ class Colour(Enum):
 
     @staticmethod
     def _sgr(code):
-        sys.stdout.write("\x1b[%sm" % code)
+        sys.stdout.write(sgr(code))
